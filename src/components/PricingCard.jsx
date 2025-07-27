7403911eb09fd3ee310acad86f9c6a8781b500fe
akshay-ken <akshaybest3120@gmail.com>
2025-07-27 18:49:59 +0530

    feat: cards can be selected now, used clsx for conditional styling

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,26 +1,58 @@
-export function PricingCard({ planName, plalnPrice, feat1, feat2, feat3 }) {
+import clsx from "clsx";
+
+export function PricingCard({
+  planName,
+  plalnPrice,
+  feat1,
+  feat2,
+  feat3,
+  currentCard,
+  handleSelect,
+}) {
+  const featuredBg = clsx({ "bg-custom-gradient": currentCard });
+  const featuredText = clsx({ "text-white": currentCard });
+  const featuredButton = clsx({
+    "bg-white text-[hsl(238,63%,64%)]": currentCard,
+    "text-white bg-custom-gradient": !currentCard,
+  });
   return (
     <>
-      <div class="rounded-xl mx-auto bg-white min-w-sm flex flex-col items-center p-6">
-        <p class="text-2xl mb-6 text-Gray-650 font-bold">{planName}</p>
+      <div
+        className={`rounded-xl mx-auto bg-white min-w-sm flex flex-col items-center p-6 ${featuredBg}`}
+      >
+        <p className={`text-2xl mb-6 text-Gray-650 font-bold ${featuredText}`}>
+          {planName}
+        </p>
 
-        <p class="text-8xl font-extrabold tracking-tight flex items-center text-Gray-700 mb-8 before:content-['$_'] before:text-5xl">
+        <p
+          className={`text-8xl font-extrabold tracking-tight flex items-center text-Gray-700 mb-8 before:content-['$_'] before:text-5xl ${featuredText}`}
+        >
           {plalnPrice}
         </p>
 
-        <p class="text-lg font-bold text-Gray-650 py-4 border-y-1 w-full text-center">
+        <p
+          className={`text-lg font-bold text-Gray-650 py-4 border-y-1 w-full text-center ${featuredText}`}
+        >
           {feat1}
         </p>
 
-        <p class="text-lg font-bold text-Gray-650 py-4 border-b-1 w-full text-center">
+        <p
+          className={`text-lg font-bold text-Gray-650 py-4 border-b-1 w-full text-center ${featuredText}`}
+        >
           {feat2}
         </p>
 
-        <p class="text-lg font-bold mb-10 text-Gray-650 py-4 border-b-1 w-full text-center">
+        <p
+          className={`text-lg font-bold mb-10 text-Gray-650 py-4 border-b-1 w-full text-center ${featuredText}`}
+        >
           {feat3}
         </p>
 
-        <button class="w-full rounded-lg text-lg font-semibold tracking-widest uppercase text-center py-4 text-white bg-custom-gradient">
+        <button
+          onClick={handleSelect}
+          className={`w-full rounded-lg text-lg font-semibold tracking-widest uppercase text-center py-4
+              ${featuredButton}`}
+        >
           Learn More
         </button>
       </div>
--- src/components/PricingPage.jsx
+++ src/components/PricingPage.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { PricingCard } from "./PricingCard";
 
 export function PricingPage() {
+  //   const [selected, setSelected] = useState(false);
+  //   function handleSelecet() {
+  //     setSelected((prevState) => !prevState);
+  //   }
+  const [selected, setSelected] = useState(0);
+  function handleSelect(planName) {
+    setSelected(planName);
+  }
   return (
     <main className="w-full h-screen flex ">
       <div className="my-auto mx-auto">
@@ -9,9 +18,9 @@ export function PricingPage() {
         </h1>
         <div className="flex flex-row my-4 gap-x-6 items-center justify-center">
           <p className="font-bold text-Gray-650 text-lg ">Annually</p>
-          <label class="inline-block cursor-pointer ">
-            <input type="checkbox" class="peer sr-only" />
-            <div class="relative w-14 h-8 bg-sky-400 rounded-full peer-checked:before:translate-x-full   before:absolute before:top-[4px] before:start-[4px] before:bg-white before:rounded-full before:h-6 before:w-6 before:transition-transform peer-checked:bg-sky-400"></div>
+          <label className="inline-block cursor-pointer ">
+            <input type="checkbox" className="peer sr-only" />
+            <div className="relative w-14 h-8 bg-sky-400 rounded-full peer-checked:before:translate-x-full   before:absolute before:top-[4px] before:start-[4px] before:bg-white before:rounded-full before:h-6 before:w-6 before:transition-transform peer-checked:bg-sky-400"></div>
           </label>
           <p className="font-bold text-Gray-650 text-lg ">Monthly</p>
         </div>
@@ -22,6 +31,8 @@ export function PricingPage() {
           feat1={"500 GB Storage"}
           feat2={"2 Users Allowed"}
           feat3={"Send up to 3 GB"}
+          currentCard={selected === 1}
+          handleSelect={() => handleSelect(1)}
         />
         <PricingCard
           planName={"Professional"}
@@ -29,6 +40,8 @@ export function PricingPage() {
           feat1={"1 TB Storage"}
           feat2={"5 Users Allowed"}
           feat3={"Send up to 10 GB"}
+          currentCard={selected === 2}
+          handleSelect={() => handleSelect(2)}
         />
         <PricingCard
           planName={"Master"}
@@ -36,6 +49,8 @@ export function PricingPage() {
           feat1={"2 TB Storage"}
           feat2={"10 Users Allowed"}
           feat3={"Send up to 20 GB"}
+          currentCard={selected === 3}
+          handleSelect={() => handleSelect(3)}
         />
       </div>
     </main>
