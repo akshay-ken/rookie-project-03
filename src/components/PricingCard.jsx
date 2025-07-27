449adc8c020e85add7afc4b5cc05123ad86d877c
akshay-ken <akshaybest3120@gmail.com>
2025-07-27 17:30:28 +0530

    feat: added props to pricingcard component

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,23 +1,23 @@
-export function PricingCard() {
+export function PricingCard({ planName, plalnPrice, feat1, feat2, feat3 }) {
   return (
     <>
       <div class="rounded-xl mx-auto bg-white min-w-sm flex flex-col items-center p-6">
-        <p class="text-2xl mb-6 text-Gray-650 font-bold">Basic</p>
+        <p class="text-2xl mb-6 text-Gray-650 font-bold">{planName}</p>
 
         <p class="text-8xl font-extrabold tracking-tight flex items-center text-Gray-700 mb-8 before:content-['$_'] before:text-5xl">
-          199.99
+          {plalnPrice}
         </p>
 
         <p class="text-lg font-bold text-Gray-650 py-4 border-y-1 w-full text-center">
-          500 GB Storage
+          {feat1}
         </p>
 
         <p class="text-lg font-bold text-Gray-650 py-4 border-b-1 w-full text-center">
-          2 Users Allowed
+          {feat2}
         </p>
 
         <p class="text-lg font-bold mb-10 text-Gray-650 py-4 border-b-1 w-full text-center">
-          Send up to 3 GB
+          {feat3}
         </p>
 
         <button class="w-full rounded-lg text-lg font-semibold tracking-widest uppercase text-center py-4 text-white bg-custom-gradient">
