e32fe39827af6e9571df1768a81e412932dcdb43
akshay-ken <akshaybest3120@gmail.com>
2025-07-27 08:11:12 +0530

    style: created two flex objects and added code of card component

---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,17 @@
+<div class="rounded-xl mx-auto bg-sky-600 max-w-sm flex flex-col items-center gap-y- p-6">
+  <p class="text-2xl mb-4 text-white">professional</p>
+
+  <p class="text-9xl font-black text-white mb-6 before:content-['$_'] before:text-6xl">
+    299
+  </p>
+
+  <p class="text-2xl text-white border-y-2 w-full text-center">text1</p>
+
+  <p class="text-2xl text-white border-b-2 w-full text-center">text1</p>
+
+  <p class="text-2xl mb-4 text-white border-b-2 w-full text-center">text1</p>
+
+  <button class="w-full rounded-lg text-center py-2 text-white bg-rose-400">
+    link click
+  </button>
+</div>;
--- src/components/PricingPage.jsx
+++ src/components/PricingPage.jsx
@@ -2,14 +2,16 @@ export function PricingPage() {
   return (
     <main className="w-full h-screen flex ">
       <div className="my-auto mx-auto">
-        <h1 className="text-4xl font-bold text-Gray-650 ">Our Pricing</h1>
+        <h1 className="text-4xl font-bold text-Gray-650 text-center ">
+          Our Pricing
+        </h1>
         <div className="flex flex-row my-4 gap-x-6 items-center justify-center">
-          <p className="font-bold text-Gray-650 text-lg">Annually</p>
-          <label class="inline-block cursor-pointer mt-2">
+          <p className="font-bold text-Gray-650 text-lg ">Annually</p>
+          <label class="inline-block cursor-pointer ">
             <input type="checkbox" class="peer sr-only" />
             <div class="relative w-14 h-8 bg-sky-400 rounded-full peer-checked:before:translate-x-full   before:absolute before:top-[4px] before:start-[4px] before:bg-white before:rounded-full before:h-6 before:w-6 before:transition-transform peer-checked:bg-sky-400"></div>
           </label>
-          <p className="font-bold text-Gray-650 text-lg">Monthly</p>
+          <p className="font-bold text-Gray-650 text-lg ">Monthly</p>
         </div>
       </div>
     </main>
